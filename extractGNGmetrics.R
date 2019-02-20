0cd17186ee92aee7c1082d8de8f786ebaa4e8d60
Isaac Kinley <Isaac Kinley>
2019-02-19 22:01:17 -0500

    More metrics

--- extractGNGmetrics.R
+++ extractGNGmetrics.R
@@ -1,5 +1,10 @@
 
 extract.GNG.metrics <- function(full.path) {
+  # Extracts the following metrics from go/no-go raw CSVs:
+  # Mean reaction time for true positives
+  # Proportion true positives
+  # Mean reaction time for false positives
+  # Proportion false positives
   data <- read.table(full.path,
                                  header = T,
                                  sep = ',',
@@ -28,20 +33,25 @@ extract.GNG.metrics <- function(full.path) {
   nogo.trials <- data$event == 'nogo'
   n.nogo <- sum(nogo.trials)
   n.false.pos <- 0
+  false.pos.rts <- c()
   for (trial.idx in which(nogo.trials)) {
     # If the following event was a response, this is a false positive
     if (trial.idx != dim(data)[1]) {
       if (data$event[trial.idx + 1] == 'response') {
         n.false.pos <- n.false.pos + 1
+        false.pos.rts <- c(false.pos.rts,
+                           data$time[trial.idx + 1] - data$time[trial.idx])
       }
     }
   }
   
   metrics <- c(mean(true.pos.rts),
                n.true.pos / n.go,
+               mean(false.pos.rts),
                n.false.pos / n.nogo)
-  names(metrics) <- c('mean_RT',
+  names(metrics) <- c('mean_true_pos_RT',
                       'p_true_pos',
+                      'mean_false_pos_RT',
                       'p_false_pos')
   return(metrics)
 }
